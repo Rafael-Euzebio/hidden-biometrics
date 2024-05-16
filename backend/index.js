d5d38eff09740185b3770c8ff39ed1ff769d6474
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-05-16 03:17:06 -0300

    fix: serve static files from backend folder
    - This change was made to keep vite .gitignore unchanged, since it ignores the dist folder by defualt

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static('dist'))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
