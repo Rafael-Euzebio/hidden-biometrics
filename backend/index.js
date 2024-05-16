9747b3b2c3c5d403a5b302a822b4068f136e65bb
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-05-16 02:35:06 -0300

    refactor: change relative path to absolute path in order to serve static files

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
