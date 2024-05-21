0b9f2947d54eb324f988447fed1c382690e71603
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-05-21 18:22:10 -0300

    feat: add basic style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import User from './components/User'
 import userConfig from './utils/userConfig'
+import './styles/reset.scss'
+import './styles/blocks/app.scss'
+import './styles/blocks/main.scss'
 
 function App () {
   const { fingerprint, userInfo } = userConfig
   return (
-    <>
-      <h1>Browser Fingerprinting</h1>
-      <p>A technique used to identify users by their browser and device characteristics, without relying on their IP address.</p>
-      <User fingerprint={fingerprint} userInfo={userInfo} />
-    </>
+    <div className="app">
+      <main className="main">
+        <h1 className="main__heading"> Browser Fingerprinting</h1>
+        <p className="main__description">A technique used to identify users by their browser and device characteristics, without relying on their IP address.</p>
+        <User fingerprint={fingerprint} userInfo={userInfo} />
+      </main>
+    </div>
   )
 }
 
--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../styles/blocks/user.scss'
 
 const UserInfo = ({ userInfo }) => {
   return (
     <div>
-      <p>These are <strong>some</strong> information used to generate that fingerprint</p>
+      <p className="main__description">These are <strong>some</strong> information used to generate that fingerprint</p>
       <ul>
         {
           Object.keys(userInfo).map(key => {
             return (
-              <li key={key}>
+              <li className="main__description" key={key}>
                 {userInfo[key].text}: {userInfo[key].value}
               </li>
             )
@@ -21,8 +22,9 @@ const UserInfo = ({ userInfo }) => {
 }
 const User = ({ fingerprint, userInfo }) => {
   return (
-    <div>
-      <p>This is yours: {fingerprint}</p>
+    <div className="user">
+      <p className="main__description">This is yours:</p>
+      <span className="user__fingerprint">{fingerprint}</span>
       <UserInfo userInfo={userInfo}/>
     </div>
   )
