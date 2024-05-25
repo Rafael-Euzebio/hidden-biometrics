9b5a874d90a4df9c2b7969960711bd63c8dddcc2
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-05-25 00:47:24 -0300

    chore: add alias paths for styles and components

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { coverageConfigDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,12 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul', // or 'v8'
       exclude: [...coverageConfigDefaults.exclude, '**/main.jsx/**', '**/App.jsx/**']
+    }
+  },
+  resolve: {
+    alias: {
+      '@styles': `${path.resolve(__dirname, './src/styles')}`,
+      '@components': `${path.resolve(__dirname, './src/components')}`
     }
   }
 })
