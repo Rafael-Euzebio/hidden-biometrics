7a35514f8db5f1927b6dcec5689783509afe1d55
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-05-28 02:33:19 -0300

    test: add tests for navbar links (github and linkedin)

--- frontend/tests/User.test.js
+++ frontend/tests/User.test.js
@@ -10,3 +10,17 @@ test('should find browser fingerprint', async ({ page }) => {
   expect(fingerprint).toBeDefined()
   expect(fingerprint).not.toBeNull()
 })
+
+test('should redirect to websites corresponding to the icons on navbar', async ({ page }) => {
+  const links = [
+    'https://github.com/Rafael-Euzebio/hidden-biometrics',
+    'https://www.linkedin.com/in/rafael-euzebio/'
+  ]
+  await page.goto('http://localhost:5173')
+
+  for (const link of links) {
+    await page.locator(`a[href="${link}"]`).click()
+    expect(page.url()).toBe(link)
+    await page.goto('http://localhost:5173')
+  }
+})
