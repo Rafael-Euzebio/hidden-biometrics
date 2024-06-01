f548d75e8d4dcf05f338299f074eb5066c84b7a6
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-01 01:27:19 -0300

    feat: add description for browser fingerprinting from a academic paper
    - The "generic" description for browser fingerprinting was replaced by
      one from a academic paper on that subject. The source was added using
      the `<Link />` component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,11 @@ import '@styles/reset.scss'
 import '@styles/blocks/app.scss'
 import '@styles/blocks/main.scss'
 import '@styles/blocks/header.scss'
+import Link from '@components/Link'
 
 function App () {
   const { fingerprint, userInfo } = userConfig
+
   return (
     <div className="app">
       <header className="header">
@@ -17,8 +19,17 @@ function App () {
         <Navbar />
       </header>
       <main className="main">
-        <h1 className="main__heading"> Browser Fingerprinting</h1>
-        <p className="main__description">A technique used to identify users by their browser and device characteristics, without relying on their IP address.</p>
+        <h2 className="main__heading"> Browser Fingerprinting</h2>
+        <p className="main__description">
+          "(...) a set of information related to a user’s device from the hardware to the operating system to the browser and its configuration (...)
+          It does not leave any trace (...) the user has no control over the collection process. <br/>
+          (...) it can be identified on the web without the need of other identifiers like a cookie or an IP address."
+        </p>
+        <Link 
+          text="Browser Fingerprinting: a survey" 
+          href="https://arxiv.org/pdf/1905.01051" 
+          modifier="link--type-text"
+        />
         <User fingerprint={fingerprint} userInfo={userInfo} />
       </main>
     </div>
