8ca9d1c91efb26e70149edd2ea8fdaaa924f6037
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-05 17:14:01 -0300

    test: change test to find user info on document
    - The test was changed because user information is now displayed in a
      collapsible component. Now it only asserts that the information is in
      the document instead of visible.
      - The visibility upon click is asserted in the Collapsible component's
        tests

--- frontend/src/components/User.test.jsx
+++ frontend/src/components/User.test.jsx
@@ -8,38 +8,6 @@ const mockUserInfo = {
   userAgent: {
     text: 'User Agent',
     value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-  },
-  browser: {
-    text: 'Browser',
-    value: 'Chrome'
-  },
-  browserVersion: {
-    text: 'Browser Version',
-    value: '124.0.0.0'
-  },
-  os: {
-    text: 'Operational System',
-    value: 'Linux'
-  },
-  OSVersion: {
-    text: 'OS Version',
-    value: 'x86_64'
-  },
-  cpu: {
-    text: 'CPU',
-    value: 'amd64'
-  },
-  resolution: {
-    text: 'Current Resolution',
-    value: '1366x768'
-  },
-  plugins: {
-    text: 'Installed Plugins',
-    value: 'PDF Viewer, Chrome PDF Viewer, Chromium PDF Viewer, Microsoft Edge PDF Viewer, WebKit built-in PDF'
-  },
-  fonts: {
-    text: 'Installed Fonts',
-    value: 'Arial Black, Arial, Bauhaus 93, Calibri, Cambria, Comic Sans MS, Courier New, Georgia, Impact, KacstOne, Lohit Gujarati, Loma, Rachana, Sawasdee, Times New Roman, Trebuchet MS, Ubuntu, Umpush, Verdana, Webdings'
   }
 }
 
@@ -48,14 +16,14 @@ beforeEach(() => {
 })
 
 describe('<User />', () => {
-  test('Should display mock user information to the user', () => {
-    for (const key in mockUserInfo) {
-      const element = screen.getByText(`${mockUserInfo[key].text}: ${mockUserInfo[key].value}`)
-      expect(element).toBeVisible()
-    }
+  test('should render mock user info', () => {
+    const text = screen.getByText(mockUserInfo.userAgent.text)
+    expect(text).toBeVisible()
+    const value = screen.getByText(mockUserInfo.userAgent.value)
+    expect(value).toBeInTheDocument()
   })
 
-  test('Should display mock fingerprint to the user', () => {
+  test('should display mock fingerprint to the user', () => {
     const element = screen.getByText(mockFingerprint, { exact: false })
     expect(element).toBeVisible()
   })
