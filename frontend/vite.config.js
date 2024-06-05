549b01126dc4345313fb3564a828fac4d12f1cd6
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-05 17:18:56 -0300

    chore: add files to exclude from unit tests
    - The files excluded from test runs and coverage statistics was:
      - Default files from vitest documentation
      - App.jsx and Main.jsx
      - e2e folder (contains playwright tests that shouldn't be executed by
        vitest)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,9 +14,15 @@ export default defineConfig({
       name: 'chrome', // browser name is required
       headless: true
     },
+    exclude: ['**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/e2e/**'],
     coverage: {
       provider: 'istanbul', // or 'v8'
-      exclude: [...coverageConfigDefaults.exclude, '**/main.jsx/**', '**/App.jsx/**']
+      exclude: [...coverageConfigDefaults.exclude, '**/main.jsx/**', '**/App.jsx/**', '**/e2e/**']
     }
   },
   resolve: {
