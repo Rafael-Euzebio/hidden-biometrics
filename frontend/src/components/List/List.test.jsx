432e851666702ccd2860eed28c8bb045bd93c43f
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-09 20:45:49 -0300

    test: refactor prop related tests
    - Now props are declared in a object as valid or invalid. The props are
      tested through the function `testPropTypes`

--- frontend/src/components/List/List.test.jsx
+++ frontend/src/components/List/List.test.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
-import { describe, test, expect, vi } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import List from './List'
+import testPropTypes from '../../test-helpers/test-helpers'
 
 describe('<List />', () => {
-  test('should render every item from the array passed as props', () => {
-    const listItems = ['item-1', 'item-2', 'item-3', 'item-4', 'item-5', 'item-6', 'item-7', 'item-8', 'item-9']
+  const validProps = {
+    items: ['item-1', 'item-2', 'item-3', 'item-4', 'item-5', 'item-6', 'item-7', 'item-8', 'item-9'],
+    modifier: 'list--horizontal'
+  }
+  const invalidProps = {
+    items: 1,
+    modifier: 1
+  }
 
-    render(<List items={listItems} />)
-    for (const item of listItems) {
+  test('should render a list with every item from the array passed as props', () => {
+    render(<List items={validProps.items} modifier={validProps.modifier}/>)
+    for (const item of validProps.items) {
       expect(screen.getByText(item)).toBeVisible()
     }
   })
 
-  test('should throw an error if props is not provided', () => {
-    const spyMock = vi.spyOn(console, 'error').mockImplementation(() => {})
-    render(<List />)
-    expect(spyMock).toHaveBeenCalled()
   })
 
-  test('should throw an error if items is not an array', () => {
-    const spyMock = vi.spyOn(console, 'error').mockImplementation(() => {})
-    render(<List items="mock"/>)
-    expect(spyMock).toHaveBeenCalled()
-  })
-
-  test('should throw an error if modifier is not a string', () => {
-    const spyMock = vi.spyOn(console, 'error').mockImplementation(() => {})
-    render(<List items={ ['1', '2', '3'] } modifier={ 1 }/>)
-    expect(spyMock).toHaveBeenCalled()
-  })
+  testPropTypes(List, validProps, invalidProps)
 })
