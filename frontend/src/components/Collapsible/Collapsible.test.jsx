8ebdbe6306704383fd6565240bb8acac0833987d
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-09 21:07:12 -0300

    test: add test for prop-types

--- frontend/src/components/Collapsible/Collapsible.test.jsx
+++ frontend/src/components/Collapsible/Collapsible.test.jsx
@@ -1,26 +1,36 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import Collapsible from './Collapsible'
+import testPropTypes from '../../test-helpers/test-helpers'
 
 describe('<Collapsible />', () => {
-  test('should render label', () => {
-    render(<Collapsible
-      label="mockLabel"
-      content="mockContent"
-    />)
+  const validProps = {
+    label: 'mockLabel',
+    content: 'mockContent'
+  }
 
+  const invalidProps = {
+    label: 1,
+    content: 2
+  }
+
+  beforeEach(() => {
+    render(<Collapsible
+      label={ validProps.label }
+      content={ validProps.content }
+    />)
+  })
+
+  test('should render label', () => {
     expect(screen.getByText('mockLabel')).toBeVisible()
   })
 
   test('should render content when clicked', () => {
-    render(<Collapsible
-      label="mockLabel"
-      content="mockContent"
-    />)
-
     const element = screen.getByText('mockLabel')
     element.click()
     expect(screen.getByText('mockContent')).toBeVisible()
   })
+
+  testPropTypes(Collapsible, validProps, invalidProps)
 })
