818974ab89e5094f44c229eafe72448003702d71
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-21 23:18:17 -0300

    test: add test for label and text content on <Link /> component
    - add test to ensure an error is thrown if neither `label` or `text` content
      is provided to the `<Link />` component
    - refactor existing test to ensure an error is not thrown in case
      `<Link />` receives one of `label` or `text`

--- frontend/src/components/Link/Link.test.jsx
+++ frontend/src/components/Link/Link.test.jsx
@@ -1,4 +1,4 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import Link from './Link'
@@ -9,6 +9,7 @@ describe('<Link />', () => {
   const validProps = {
     icon: <GitHub data-testid="icon"/>,
     text: 'Github',
+    label: 'github link',
     href: 'https://github.com',
     modifier: 'link--type-icon'
   }
@@ -16,19 +17,33 @@ describe('<Link />', () => {
   const invalidProps = {
     icon: 1,
     text: 1,
+    label: 1,
     href: 1,
     modifier: 1
   }
+
+  const spy = vi.spyOn(console, 'error').mockImplementation(() => {})
 
   test('should render link and icon', async () => {
     render(<Link
       icon={validProps.icon}
       text={validProps.text}
+      label={validProps.label}
       href={validProps.href}
     />)
 
     expect(screen.getByTestId('icon')).toBeVisible()
     expect(screen.getByText('Github')).toBeVisible()
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  test('should throw error if neither text content or label are provided', async () => {
+    render(<Link
+      icon={validProps.icon}
+      href={validProps.href}
+    />)
+
+    expect(spy).toHaveBeenCalled()
   })
 
   testPropTypes(Link, validProps, invalidProps)
