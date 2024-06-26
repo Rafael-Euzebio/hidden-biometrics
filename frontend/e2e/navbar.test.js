afe7946a11fd26cbffb81d6316e0e9a32a750efe
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-06-25 23:19:58 -0300

    test: add tests for navbar responsiveness on mobile and desktop
    - test navbar and menu button behavior based on if browser is mobile or not
    - rename file to `navbar.test.js`

---
+++ frontend/e2e/navbar.test.js
@@ -0,0 +1,60 @@
+import { test, expect } from '@playwright/test'
+import { url } from './constants'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(url)
+})
+
+test.describe('navbar visibility', () => {
+  test.describe('desktop', () => {
+    test('should display navbar', async ({ page }) => {
+      const navbar = page.getByRole('navigation')
+      await expect(navbar).toBeVisible()
+    })
+  })
+
+  test.describe('mobile', () => {
+    test('should display menu button', async ({ page }) => {
+      const menu = page.getByLabel('menu')
+      await expect(menu).toBeVisible()
+    })
+
+    test('should only display navbar after clicking menu button', async ({ page }) => {
+      const navbar = page.getByRole('navigation')
+      await expect(navbar).not.toBeVisible()
+      const menu = page.getByLabel('menu')
+      await menu.click()
+      await expect(navbar).toBeVisible()
+    })
+  })
+})
+
+test.describe('navbar links', () => {
+  test.beforeEach(async ({ page, isMobile }) => {
+    if (isMobile) {
+      const menu = page.getByLabel('menu')
+      await menu.click()
+    }
+  })
+
+  test('should find and access Github link', async ({ page }) => {
+    const githubLink = page.getByLabel('Github')
+    await expect(githubLink).toBeVisible()
+
+    await githubLink.click()
+    expect(page.url()).toBe('https://github.com/Rafael-Euzebio/hidden-biometrics')
+  })
+
+  test('should find and access LinkedIn link', async ({ page }) => {
+    const linkedinLink = page.getByLabel('LinkedIn')
+    await expect(linkedinLink).toBeVisible()
+
+    await linkedinLink.click()
+    expect(page.url()).toBe('https://www.linkedin.com/in/rafael-euzebio/')
+  })
+
+  test('should find email link', async ({ page }) => {
+    const emailLink = page.getByLabel('Email')
+    await expect(emailLink).toBeVisible()
+  })
+})
