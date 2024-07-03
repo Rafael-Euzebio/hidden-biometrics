3da58af4c8410c38309e20bf7bd58b80fb1894e7
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-03 16:09:54 -0300

    test(e2e): refactor to use a `urls` object with the specific url necessary
    - the object contains base urls and urls with language subdomains

--- frontend/e2e/constants.js
+++ frontend/e2e/constants.js
@@ -1,4 +1,9 @@
-const url = 'http://localhost:5173'
+const urls = {
+  base: 'http://localhost:5173',
+  en: 'http://en.localhost:5173',
+  pt: 'http://pt.localhost:5173'
+}
+
 const mobileBrowsers = {
   chromium: 'Chrome',
   webkit: 'Mobile Safari'
@@ -8,5 +13,21 @@ const desktopBrowsers = {
   firefox: 'Firefox',
   webkit: 'Safari'
 }
+const translations = {
+  en: {
+    heading: 'Browser Fingerprint',
+    description: 'a set of information',
+    research: 'Browser Fingerprinting: A survey',
+    boxUpper: 'Your Fingerprint:',
+    boxLower: 'Try switching to an anonymous tab'
+  },
+  pt: {
+    heading: 'Impressão Digital de Navegador',
+    description: 'um conjunto de informações',
+    research: 'Impressão Digital de Navegador: Uma pesquisa',
+    boxUpper: 'Sua Impressão Digital:',
+    boxLower: 'Tente trocar para uma aba anônima'
+  }
+}
 
-export { url, desktopBrowsers, mobileBrowsers }
+export { urls, desktopBrowsers, mobileBrowsers, translations }
--- frontend/e2e/navbar.test.js
+++ frontend/e2e/navbar.test.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
-import { url } from './constants'
+import { urls } from './constants'
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(url)
+  await page.goto(urls.base)
 })
 
 test.describe('navbar visibility', () => {
