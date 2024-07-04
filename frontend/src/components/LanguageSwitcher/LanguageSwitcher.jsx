2ca93cc4198d9b902ef00674208ba0ab4c525724
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-04 04:28:08 -0300

    feat: add language switcher
    - set `background-color` on `navbar__item` so it doesn't has to be set
      on the added select button or any other elements that may be added in
      the future, avoiding repetition

---
+++ frontend/src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import i18n from '../../i18n'
+import '@styles/blocks/select.scss'
+import '@styles/blocks/link.scss'
+import '@styles/blocks/navbar.scss'
+
+const LanguageSwitcher = () => {
+  const changeLanguage = (event) => {
+    i18n.changeLanguage(event.target.value)
+  }
+  return (
+    <select className="select" onChange={changeLanguage} value={i18n.resolvedLanguage}>
+      <option className="select__option" value="pt">&#x1F1E7;&#x1F1F7; Português</option>
+      <option className="select__option" value="en"> &#x1F1FA;&#x1F1F8; English</option>
+    </select>
+  )
+}
+
+export default LanguageSwitcher
