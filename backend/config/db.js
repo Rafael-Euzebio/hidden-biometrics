07440f62ce1f6c7ddadcf6d93268bef0b2904627
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-28 01:59:29 -0300

    feat: add database connection config file

---
+++ backend/config/db.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+
+let mockDB = null
+
+const connectDB = async () => {
+  try {
+    let dbURL = process.env.MONGODB_URL
+
+    if (process.env.NODE_ENV === 'test') {
+      mockDB = await MongoMemoryServer.create({
+        binary: {
+          version: '4.4.17'
+        }
+      })
+      dbURL = mockDB.getUri()
+    }
+
+    const connection = mongoose.connect(dbURL)
+
+    console.log(`MongoDB connected: ${(await connection).connection.host}`)
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.connection.close()
+    if (mockDB) {
+      await mockDB.stop()
+    }
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+module.exports = { connectDB, disconnectDB}
