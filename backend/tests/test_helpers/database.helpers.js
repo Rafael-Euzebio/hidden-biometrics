37916bb3136b37872341c6805e79ce38b4ee746c
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-29 00:20:41 -0300

    test(helper): export initial user from db helper
    - Allows value multiple asserts following DRY

--- backend/tests/test_helpers/database.helpers.js
+++ backend/tests/test_helpers/database.helpers.js
@@ -1,15 +1,16 @@
 require('module-alias/register')
 const User = require('@models/user')
 
+const initialUser = {
+  fingerprint: '2297231874',
+  os: 'Windows',
+  browser: 'Firefox',
+  ip: '192.168.256.1',
+}
 const insertInitialUser = () => {
-  const user = new User({
-    fingerprint: '2297231874',
-    os: 'Windows',
-    browser: 'Firefox',
-    ip: '192.168.256.1',
-  })
+  const user = new User(initialUser)
 
   user.save()
 }
 
-module.exports = { insertInitialUser }
+module.exports = { insertInitialUser, initialUser }
