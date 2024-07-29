1dd674512f578f9c86a2413997ddcf644137c494
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-29 03:59:06 -0300

    feat: return user data from database
    - returns user data and status code 200 on a succesful request
    - returns 400 if user fingerprint isn't in the database

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,23 @@ const express = require('express')
 const app = express()
 const port = process.env.NODE_ENV === "test" ? 
   process.env.TEST_PORT : process.env.PORT
+const { connectDB } = require('@config/db')
+const User = require('@models/user')
 
 app.use(express.static('dist'))
+app.use(express.json())
+
+connectDB()
+
+app.get('/api/users/:fingerprint', async (req, res) => {
+  const fingerprint = req.params.fingerprint
+  const user = await User.findOne({ fingerprint })
+  if (user) {
+    return res.json({ user }).status(200).end()
+  } else {
+    return res.status(404).end()
+  }
+})
 
 const server = app.listen(port, () => {
   console.log(`Running on port ${port}`)
