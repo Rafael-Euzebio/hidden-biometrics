f5c030f58c3b4fa8e4d03295dd09b394fb3eae02
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-30 01:48:42 -0300

    test: assert `get` request to `api/users:fingerprint` returns expected information
    - returns `user` and `200` on a succesful request
    - returns `400` if user does not exist in the database

---
+++ backend/tests/integration/users.test.js
@@ -0,0 +1,37 @@
+require('module-alias/register')
+const supertest = require('supertest')
+const { app, server } = require('../../app')
+const request = supertest(app)
+const { connectDB, disconnectDB } = require('@config/db.js')
+const { initializeDB, initialUser } = require('@tests/test_helpers/database.helpers.js')
+const { it, describe, beforeEach, afterEach } = require('node:test')
+const assert = require('node:assert/strict')
+
+
+describe('Users route', () => {
+  beforeEach(async () => {
+    await connectDB()
+  })
+
+  afterEach(async () => {
+    await disconnectDB()
+    server.close()
+  })
+
+  describe('GET', () => {
+    it('should return user and 200 when user is in database', async () => {
+      await initializeDB()
+      const res = await request.get(`/api/users/${initialUser.fingerprint}`)
+      const { user } = res.body
+
+      assert.equal(user.fingerprint, initialUser.fingerprint)
+      assert.equal(res.status, 200)
+    })
+
+    it('should return 404 when the user is not in the database', async () => {
+      const res = await request.get(`/api/users/${initialUser.fingerprint}`)
+      assert.equal(res.status, 404)
+    })
+  })
+
+})
