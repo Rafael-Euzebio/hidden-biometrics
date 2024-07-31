d5925bbbae05389d4565137ab78ba03b8e24a044
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-07-31 03:04:38 -0300

    fix: prevent multiple connection attempts
    - only attempts to connect if none was yet estabilished
      - allows `connectDB` to be called before each test

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,29 +2,34 @@ const mongoose = require('mongoose')
 const { MongoMemoryServer } = require('mongodb-memory-server')
 
 let mockDB = null
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    let dbURL = process.env.MONGODB_URL
+  if (!isConnected) {
+    isConnected = true
+    try {
+      let dbURL = process.env.MONGODB_URL
 
-    if (process.env.NODE_ENV === 'test') {
-      mockDB = await MongoMemoryServer.create({
-        binary: {
-          version: '4.4.17'
-        }
-      })
-      dbURL = mockDB.getUri()
+      if (process.env.NODE_ENV === 'test') {
+        mockDB = await MongoMemoryServer.create({
+          binary: {
+            version: '4.4.17'
+          }
+        })
+        dbURL = mockDB.getUri()
+      }
+
+      const connection = mongoose.connect(dbURL)
+
+      console.log(`MongoDB connected: ${(await connection).connection.host}`)
+    } catch(error) {
+      console.error(error)
     }
-
-    const connection = mongoose.connect(dbURL)
-
-    console.log(`MongoDB connected: ${(await connection).connection.host}`)
-  } catch(error) {
-    console.error(error)
   }
 }
 
 const disconnectDB = async () => {
+  isConnected = false
   try {
     await mongoose.connection.close()
     if (mockDB) {
