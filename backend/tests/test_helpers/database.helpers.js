e051f61ff3952cb1018b5f1262580ade5c54c89a
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-08-02 03:37:06 -0300

    test(helpers): add invalid user object to test invalid requests

--- backend/tests/test_helpers/database.helpers.js
+++ backend/tests/test_helpers/database.helpers.js
@@ -7,10 +7,17 @@ const initialUser = {
   browser: 'Firefox',
   ip: '192.168.256.1',
 }
+
+const invalidUser = {
+  fingerprint: '2297231874',
+  os: 'Windows',
+  ip: '192.168.256.1',
+}
+
 const initializeDB = async () => {
   const user = new User(initialUser)
 
   await user.save()
 }
 
-module.exports = { initializeDB, initialUser }
+module.exports = { initializeDB, initialUser, invalidUser}
