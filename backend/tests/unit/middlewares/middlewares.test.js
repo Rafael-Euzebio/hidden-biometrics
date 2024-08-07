4968e4dff502a59086c0d4a462584a7449425b46
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-08-07 03:37:17 -0300

    test: assert standardResponse sends correct data and status codes

---
+++ backend/tests/unit/middlewares/middlewares.test.js
@@ -0,0 +1,67 @@
+require('module-alias/register')
+const { standardResponse } = require('@middlewares/middlewares')
+const { mock, it, describe, beforeEach, afterEach } = require('node:test')
+const httpMocks = require('node-mocks-http')
+const assert = require('node:assert/strict')
+
+
+describe('standardResponse middleware', () => {
+  let req, res, next
+  beforeEach(() => {
+    req = httpMocks.createRequest({})
+    res = httpMocks.createResponse({})
+    next = mock.fn(() => {})
+    standardResponse(req, res, next)
+  })
+
+  afterEach(() => {
+    req = res = next = null
+  })
+
+  it('should find function standardResponse in res object', async () => {
+    assert.ok(res.standardResponse)
+  })
+
+  describe('when response is successful', () => {
+    beforeEach(() => {
+      res.standardResponse(200, 'Success', 'mockData')
+    })
+    
+    it('should send 200 status code', () => {
+      const status = res._getStatusCode()
+      assert.equal(status, 200)
+    }) 
+
+    it('should send correct fields in body', () => {
+      const body = JSON.parse(res._getData())
+      assert.ok(body)
+      assert.equal(body.message, 'Success')
+      assert.equal(body.data, 'mockData')
+    }) 
+
+    it('should not send errors', () => {
+      const body = JSON.parse(res._getData())
+      assert.equal(body.errors, null)
+    }) 
+  })
+
+  describe('when response fails', () => {
+    beforeEach(() => {
+      res.standardResponse(400, 'Error', null, ['error1', 'error2'])
+    })
+
+    it('should send 400 status code', () => {
+      const status = res._getStatusCode()
+      assert.equal(status, 400)
+    })
+
+    it('should send errors array in body', () => {
+      const expectedErrors = ['error1', 'error2']
+      const { errors }= JSON.parse(res._getData())
+
+      for (const index in expectedErrors) {
+        assert.equal(errors[index], expectedErrors[index])
+      }
+    })
+  })
+})
