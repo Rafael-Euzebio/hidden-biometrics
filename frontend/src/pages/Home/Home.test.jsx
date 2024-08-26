773ca238bddc6b5ea8ee308e28bc4fc12b43a355
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-08-26 04:51:19 -0300

    test: group existent tests under `Rendering` describe block

--- frontend/src/pages/Home/Home.test.jsx
+++ frontend/src/pages/Home/Home.test.jsx
@@ -1,28 +1,34 @@
-import { describe, test, expect, beforeEach } from 'vitest'
-import { screen } from '@testing-library/react'
+import React from 'react'
+import { describe, test, expect, beforeEach, afterEach } from 'vitest'
+import { screen, render } from '@testing-library/react'
 import Home from './Home'
 import { renderWithTranslation } from '@test-helpers/test-helpers'
 import i18n from '@src/i18n.js'
 
 describe('<Home />', () => {
-  beforeEach(() => {
-    renderWithTranslation(Home)
+  describe('Rendering', () => {
+    beforeEach(() => {
+      renderWithTranslation(Home)
+    })
+
+    test('should display main description from english resource', () => {
+      const mainDescription = i18n.getDataByLanguage('en')
+        .translation
+        .main
+        .description
+
+      expect(screen.getByText(mainDescription)).toBeVisible()
+    })
+
+    test('should display main heading-2 from english resource', () => {
+      const mainHeading2 = i18n.getDataByLanguage('en')
+        .translation
+        .main['heading-2']
+
+      expect(screen.getByText(mainHeading2)).toBeVisible()
+    })
   })
 
-  test('should display main description from english resource', () => {
-    const mainDescription = i18n.getDataByLanguage('en')
-      .translation
-      .main
-      .description
 
-    expect(screen.getByText(mainDescription)).toBeVisible()
-  })
-
-  test('should display main heading-2 from english resource', () => {
-    const mainHeading2 = i18n.getDataByLanguage('en')
-      .translation
-      .main['heading-2']
-
-    expect(screen.getByText(mainHeading2)).toBeVisible()
   })
 })
