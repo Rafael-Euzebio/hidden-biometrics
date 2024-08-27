a69ae3ec1357f4663fbd9efc3292b1e02783ccfa
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-08-27 03:44:44 -0300

    feat: add translations on `Error` component
    - `error.statusMessage` is used as the translation key
      - In case it is not found, it defaults to `error.message`

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '@styles/blocks/box.scss'
 import { useRouteError } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 const Error = () => {
+  const { t } = useTranslation()
   const error = useRouteError()
   console.log(error)
   return (
     <main className="main main--centered">
       <div className="box">
-        <p className="box__content">Oops! An error happened!</p>
+        <p className="box__content">{t('errorBox.upper')}</p>
         <p className="box__content--large">{error.status}</p>
-        <p className="box__content">{error.statusText}</p>
+        <p className="box__content">{t(`errorBox.lower.${error.statusText}`, error.status)}</p>
       </div>
     </main>
   )
