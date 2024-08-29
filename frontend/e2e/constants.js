767013ee620413f1b59af354b98471e9b9958906
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-08-29 20:35:37 -0300

    test(e2e): assert error page displays message in correct language

--- frontend/e2e/constants.js
+++ frontend/e2e/constants.js
@@ -10,21 +10,31 @@ const desktopBrowsers = {
   webkit: 'Safari'
 }
 const translations = {
-  en: {
-    heading: 'Browser Fingerprint',
-    description: 'a set of information',
-    research: 'Browser Fingerprinting: A survey',
-    boxUpper: 'Your Fingerprint:',
-    boxLowerVPN: 'Try switching to an anonymous tab',
-    boxLowerAccessCount: 'You have viewed this page'
+  home: {
+    en: {
+      heading: 'Browser Fingerprint',
+      description: 'a set of information',
+      research: 'Browser Fingerprinting: A survey',
+      boxUpper: 'Your Fingerprint:',
+      boxLowerVPN: 'Try switching to an anonymous tab',
+      boxLowerAccessCount: 'You have viewed this page'
+    },
+    pt: {
+      heading: 'Impressão Digital de Navegador',
+      description: 'um conjunto de informações',
+      research: 'Impressão Digital de Navegador: Uma pesquisa',
+      boxUpper: 'Sua Impressão Digital:',
+      boxLowerVPN: 'Tente trocar para uma aba anônima',
+      boxLowerAccessCount: 'Você visualizou essa página'
+    }
   },
-  pt: {
-    heading: 'Impressão Digital de Navegador',
-    description: 'um conjunto de informações',
-    research: 'Impressão Digital de Navegador: Uma pesquisa',
-    boxUpper: 'Sua Impressão Digital:',
-    boxLowerVPN: 'Tente trocar para uma aba anônima',
-    boxLowerAccessCount: 'Você visualizou essa página'
+  error: {
+    en: {
+      errorMessage: 'Oops! An error happened!'
+    },
+    pt: {
+      errorMessage: 'Oops! Ocorreu um erro!'
+    }
   }
 }
 
--- frontend/e2e/i18n.test.js
+++ frontend/e2e/i18n.test.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 import { url, translations } from './constants'
 
-const translationTests = (language) => {
-  const translation = translations[language]
+const homeTests = (language) => {
+  const translation = translations.home[language]
 
   test(`should display h2 in ${language}`, async ({ page }) => {
     const heading = page.getByRole('heading', { name: translation.heading })
@@ -29,59 +29,127 @@ const translationTests = (language) => {
   })
 }
 
+const errorTests = (language) => {
+  const translation = translations.error[language]
+
+  test(`should display error message in ${language}`, async ({ page }) => {
+    const errorMessage = page.getByText(translation.errorMessage)
+    await expect(errorMessage).toBeVisible()
+  })
+}
+
 test.describe('internationalization', () => {
-  for (const language in translations) {
-    test.describe((`when locale is set to ${language}`), () => {
+  test.describe('Home', () => {
+    for (const language in translations.home) {
+      test.describe((`when locale is set to ${language}`), () => {
+        test.beforeEach(async ({ page }) => {
+          await page.goto(url)
+        })
+
+        test.use({
+          locale: language
+        })
+
+        homeTests(language)
+      })
+
+      test.describe((`when querystring is ${language}`), () => {
+        test.beforeEach(async ({ page }) => {
+          await page.goto(`${url}?lng=${language}`)
+        })
+
+        homeTests(language)
+      })
+
+      test.describe((`when user selects ${language}`), () => {
+        test.describe('desktop', () => {
+          test.beforeEach(async ({ page }) => {
+            await page.goto(url)
+            const languageSwitcher = page.getByRole('combobox')
+            await languageSwitcher.selectOption(language)
+          })
+
+          homeTests(language)
+        })
+
+        test.describe('mobile', () => {
+          test.beforeEach(async ({ page }) => {
+            await page.goto(url)
+            const menu = page.getByLabel('menu button')
+            await menu.click()
+            const languageSwitcher = page.getByRole('combobox')
+            await expect(languageSwitcher).toBeVisible()
+            await languageSwitcher.selectOption(language)
+          })
+
+          homeTests(language)
+        })
+      })
+    }
+
+    test.describe(('fallbacks to en when subdomain or locale is not set'), () => {
       test.beforeEach(async ({ page }) => {
         await page.goto(url)
       })
 
-      test.use({
-        locale: language
+      homeTests('en')
+    })
+  })
+
+  test.describe('Error Page', () => {
+    for (const language in translations.error) {
+      test.describe((`when locale is set to ${language}`), () => {
+        test.beforeEach(async ({ page }) => {
+          await page.goto(`${url}/unknown`)
+        })
+
+        test.use({
+          locale: language
+        })
+
+        errorTests(language)
       })
 
-      translationTests(language)
-    })
+      test.describe((`when querystring is ${language}`), () => {
+        test.beforeEach(async ({ page }) => {
+          await page.goto(`${url}/unknown?lng=${language}`)
+        })
 
-    test.describe((`when querystring is ${language}`), () => {
+        errorTests(language)
+      })
+
+      test.describe((`when user selects ${language}`), () => {
+        test.describe('desktop', () => {
+          test.beforeEach(async ({ page }) => {
+            await page.goto(`${url}/unknown`)
+            const languageSwitcher = page.getByRole('combobox')
+            await languageSwitcher.selectOption(language)
+          })
+
+          errorTests(language)
+        })
+
+        test.describe('mobile', () => {
+          test.beforeEach(async ({ page }) => {
+            await page.goto(url)
+            const menu = page.getByLabel('menu button')
+            await menu.click()
+            const languageSwitcher = page.getByRole('combobox')
+            await expect(languageSwitcher).toBeVisible()
+            await languageSwitcher.selectOption(language)
+          })
+
+          errorTests(language)
+        })
+      })
+    }
+
+    test.describe(('fallbacks to en when subdomain or locale is not set'), () => {
       test.beforeEach(async ({ page }) => {
-        await page.goto(`${url}?lng=${language}`)
+        await page.goto(`${url}/unknown`)
       })
 
-      translationTests(language)
+      errorTests('en')
     })
-
-    test.describe((`when user selects ${language}`), () => {
-      test.describe('desktop', () => {
-        test.beforeEach(async ({ page }) => {
-          await page.goto(url)
-          const languageSwitcher = page.getByRole('combobox')
-          await languageSwitcher.selectOption(language)
-        })
-
-        translationTests(language)
-      })
-
-      test.describe('mobile', () => {
-        test.beforeEach(async ({ page }) => {
-          await page.goto(url)
-          const menu = page.getByLabel('menu button')
-          await menu.click()
-          const languageSwitcher = page.getByRole('combobox')
-          await expect(languageSwitcher).toBeVisible()
-          await languageSwitcher.selectOption(language)
-        })
-
-        translationTests(language)
-      })
-    })
-  }
-
-  test.describe(('fallbacks to en when subdomain or locale is not set'), () => {
-    test.beforeEach(async ({ page }) => {
-      await page.goto(url)
-    })
-
-    translationTests('en')
   })
 })
