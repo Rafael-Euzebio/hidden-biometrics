50e1f8aa67f762b805fe243269b5864a2a4c2259
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-09-03 17:04:15 -0300

    fix: fix language switcher not re-rendering on value change
    - Adds state to `LanguageSwitcher`, making it a controlled component,
      thus triggering re-rendering on value change.

--- frontend/src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ frontend/src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import i18n from '../../i18n'
 import '@styles/blocks/select.scss'
 import '@styles/blocks/link.scss'
 import '@styles/blocks/navbar.scss'
 
 const LanguageSwitcher = () => {
+  const [selectedLanguage, setselectedLanguage] = useState(i18n.resolvedLanguage)
+
   const changeLanguage = (event) => {
-    i18n.changeLanguage(event.target.value)
+    const language = event.target.value
+    setselectedLanguage(language)
+    i18n.changeLanguage(language)
   }
+
   return (
-    <select className="select" onChange={changeLanguage} value={i18n.resolvedLanguage} aria-label="language selector">
+    <select className="select" onChange={changeLanguage} value={selectedLanguage} aria-label="language selector">
       <option className="select__option" value="pt">&#x1F1E7;&#x1F1F7; Português</option>
       <option className="select__option" value="en"> &#x1F1FA;&#x1F1F8; English</option>
     </select>
