90b815af81c7c282d2855eb482f57714b7f80cc4
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-09-14 03:56:40 -0300

    feat: add charts for OS and Browsers statistics

--- frontend/src/pages/Statistics/Statistics.jsx
+++ frontend/src/pages/Statistics/Statistics.jsx
@@ -1,9 +1,64 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import 'chart.js/auto'
+import { Doughnut } from 'react-chartjs-2'
+import { useTranslation } from 'react-i18next'
 import requests from '@utils/apiDataFetcher'
 import '@styles/blocks/main.scss'
+import '@styles/blocks/charts.scss'
+import '@styles/blocks/box.scss'
+
+const Chart = ({Type, title, data, t}) => {
+  const colors = ['#F92672', '#A6E22E', '#66D9EF', '#FD971F', '#E6DB74', '#AE81FF', '#FF6F61', '#DC143C']
+
+  return (
+    <>
+      <div className="charts-wrapper__box">
+        <Type className="charts-wrapper__chart" 
+          data={{
+            labels: Object.keys(data),
+            datasets: [{
+              label: t('statistics.users'),
+              data: Object.values(data),
+              borderWidth: 1,
+              backgroundColor: colors
+            }],
+          }} 
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            color: '#F8F8F2',
+            plugins: {
+              title: {
+                display: true,
+                text: title,
+                color: '#F8F8F2',
+                font: {
+                  size: 18,
+                },
+                padding: {
+                  top: 0,
+                  bottom: 0
+                }
+              },
+              legend: {
+                labels: {
+                  font: {
+                    size: 16
+                  }
+                }
+              }
+            } 
+          }
+        }/>
+      </div>
+    </>
+  )
+}
 
 const Statistics = () => {
-  const [statistics, setstatistics] = useState({})
+  const [statistics, setstatistics] = useState(null)
+  const { t } = useTranslation()
 
   useEffect(() => {
     (async () => {
@@ -14,13 +69,26 @@ const Statistics = () => {
         console.log(error)
       }
     })()
-  })
+  }, [])
 
   return (
     <main className="main">
-      <h2 className="main__heading">Statistics</h2>
+      { statistics !== null ?
+        (
+        <div className="charts-wrapper">
+          <Chart Type={Doughnut} title={t('statistics.browsers')} data={statistics.browsers} t={t} />
+          <Chart Type={Doughnut} title={t('statistics.os')} data={statistics.os} t={t} />
+        </div>
+        ) :
+        <div></div>
+      }
     </main>
   )
 }
 
+Chart.propTypes = {
+  Type: PropTypes.elementType.isRequired,
+  title: PropTypes.string.isRequired,
+  data: PropTypes.object.isRequired
+}
 export default Statistics
