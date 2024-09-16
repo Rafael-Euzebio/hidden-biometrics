b21b1a01ea81b5716dc1585b95245f9cda18f977
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-09-16 18:30:31 -0300

    test(e2e): assert page links are visible and lead to their respective pages

--- frontend/e2e/links.test.js
+++ frontend/e2e/links.test.js
@@ -20,23 +20,37 @@ test.beforeEach(async ({ page }) => {
 })
 
 test.describe('links', () => {
-  test.describe('navbar', () => {
+
+  test.describe('Navbar', () => {
     test.beforeEach(async ({ page, isMobile }) => {
       if (isMobile) {
         const menu = page.getByLabel('menu')
         await menu.click()
       }
     })
-    for (const link of links.contact) {
-      verifyAndAccessLink('label', link.label, link.href)
-    }
+
+    test.describe('Contact links', () => {
+      for (const link of links.contact) {
+        verifyAndAccessLink('text', link.label, link.href)
+      }
+    })
+
+    test.describe('Page links', () => {
+      for (const link of links.pages) {
+        test(`should find an access ${link.text} link`, async ({ page }) => {
+          const foundLink = page.getByText(link.text)
+          expect(foundLink).toBeVisible()
+          await foundLink.click() 
+          expect(page.url()).toBe(url + link.href)
+        })
+      }
+    })
   })
 
   test.describe('footer', () => {
-    for (const linkGroup in links) {
-      for (const link of links[linkGroup]) {
-        verifyAndAccessLink('text', link.label, link.href)
-      }
+    const footerLinks = [...links.contact, ...links.techstack]
+    for (const link of footerLinks) {
+      verifyAndAccessLink('text', link.text, link.href)
     }
   })
 })
